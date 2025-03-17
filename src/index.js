7d0b9570d4fb64bd9c60e694167acf56c5b87378
Saurabh <ksaurabh2005@gmail.com>
2025-03-18 01:37:27 +0530

    utils ApiErrors and ApiResponse

--- src/index.js
+++ src/index.js
@@ -5,4 +5,19 @@ dotenv.config({
     path: "./env"
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error)=>{
+        console.log("ERRR: ", error);
+        throw error;        
+    })
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port----> ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+        console.log(`MONGODB connection failed !!! `, err);
+        
+})
