9916fcd3261f4bc83c38abe775441e75cf5fd2bc
Saurabh <ksaurabh2005@gmail.com>
2025-03-20 19:14:52 +0530

    add user route, register controller & bug fixes

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -7,7 +7,7 @@ class ApiError extends Error {
     ){
         super(message)
         this.statusCode = statusCode
-        this.message = message
+        this.message = "⭕"+message
         this.data = null
         this.success = false
         this.errors = errors
